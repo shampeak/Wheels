4221ef31fea73bccc8ceca0bdcc13b87e5fc308c
shampeak <976107540@qq.com>
2016-07-08 16:26:10 +0800

    0.0001

---
+++ App/Config/Vo.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ |------------------------------------------------
+ | 类文件和配置文件注册
+ |------------------------------------------------
+ */
+
+return [
+
+    'FileReflect'  => [
+        'Config'   => '../App/Config/Config.php',
+        'Dsn'      => '../App/Config/Dsn.php',
+        'Smarty'   => '../App/Config/Smarty.php',
+        'Db'       => '../App/Config/Db.php',
+        'Cookies'  => '../App/Config/Cookies.php',
+        'Adminauth'  => '../App/Config/Adminauth.php',
+    ],
+
+    'Providers'=>[
+        'Wise'      => Grace\Wise\Wise::class,           //memcache对象
+        'Req'       => Grace\Req\Req::class,             //
+        'View'      => Grace\View\View::class,           //
+        'Smarty'    => Grace\Smarty\Smarty::class,           //
+        'Db'        => Lulu\Db\Db::class,
+        'Table'     => Lulu\Table\Table::class,
+        'Markdown'  => Michelf\Markdown::class,
+        'Cookies'   => Grace\Cookies\Cookies::class,
+        'Adminauth' => Grace\Adminauth\Adminauth::class,
+        'Parsedown' => Parsedown::class,
+//        'Db'        => Grace\Db\Db::class,           //
+    ],
+
+];
